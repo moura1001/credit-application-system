b6ee3e8eb66cdbfa10f2b676247289503dc6e604
moura1001 <gmouraneto01@gmail.com>
2023-12-18 19:34:12 -0300

    feat: implementação das regras de negócio para solicitação de empréstimo propostas no desafio

--- src/main/kotlin/moura1001/creditapplicationsystem/dto/CreditDto.kt
+++ src/main/kotlin/moura1001/creditapplicationsystem/dto/CreditDto.kt
@@ -1,20 +1,24 @@
 package moura1001.creditapplicationsystem.dto
 
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
 import moura1001.creditapplicationsystem.domain.Credit
+import moura1001.creditapplicationsystem.exception.BusinessException
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
     @field:NotNull @field:Positive val creditValue: BigDecimal = BigDecimal.ZERO,
     @field:NotNull @field:Future val dayFirstInstallment: LocalDate = LocalDate.now().plusDays(30),
-    @field:NotNull @field:Positive val numberOfInstallments: Int = 0,
+    @field:NotNull @field:Positive @field:Max(value = 48) val numberOfInstallments: Int = 0,
     @field:NotNull @field:Positive val customerId: Long = -1
 ) {
     fun toEntity(): Credit {
         // validations
+        if (dayFirstInstallment.isAfter(LocalDate.now().plusMonths(3)))
+            throw BusinessException("entrada $dayFirstInstallment é inválida. Data da primeira parcela deve ser no máximo 3 meses após o dia atual.")
 
         return Credit(
             creditValue = this.creditValue,
